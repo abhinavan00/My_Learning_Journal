537aeda8336aaeaeaa4b23a0b1a7234ca77b405c
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-02 11:22:37 +0530

    refactor the code
    Added utility.js and utility.css files for for better structure of code and single source to manage things for multiple pages.

--- home/index.js
+++ home/index.js
@@ -1,24 +1,4 @@
-import articles from "../articles.js";
-import { navAndFooter } from "../utility.js";
-
-
-function getArticles() {
-   return articles.map(article => {
-        const {name, date, img, body, alt} = article
-        return `
-            <div class="blog-post">
-                <img class="blog-img" src="../images/${img}" alt="${alt}">
-                <div>
-                    <p class="blog-date">${date}</p>
-                    <h2>${name}</h2>
-                    <p class="blog-body">
-                        ${body}
-                    </p>
-                </div>
-            </div>
-        `
-    }).join('')
-}
+import { navAndFooter, getArticles } from "../utility.js";
 
 function render() {
     navAndFooter()
--- utility.js
+++ utility.js
@@ -1,3 +1,5 @@
+import articles from "./articles.js"
+
 export function navAndFooter() {
     document.querySelector('nav').innerHTML = `
         <div class="logo-container">
@@ -5,7 +7,7 @@ export function navAndFooter() {
             <p class="logo-title">My Learning Journal</p>
         </div>
         <div class="nav-options-container">
-            <a class="nav-links" href="">Home</a>
+            <a class="nav-links" href="../home/index.html">Home</a>
             <a class="nav-links" href="">About me</a>
         </div>
     `
@@ -13,4 +15,22 @@ export function navAndFooter() {
         <p class="footer-title" >My Learning Journal</p>
         <p class="copyright">Copyright © 2026</p>
     `
+}
+
+export function getArticles() {
+   return articles.map(article => {
+        const {name, date, img, body, alt} = article
+        return `
+            <div class="blog-post">
+                <img class="blog-img" src="../images/${img}" alt="${alt}">
+                <div>
+                    <p class="blog-date">${date}</p>
+                    <h2>${name}</h2>
+                    <p class="blog-body">
+                        ${body}
+                    </p>
+                </div>
+            </div>
+        `
+    }).join('')
 }

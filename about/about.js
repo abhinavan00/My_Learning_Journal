1da4d55482067599e0cbef1bec2ed6765b52b500
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-08 05:56:23 +0530

    Added view more btn for small screen
    on home page and about page, will remove about page tomorrow.

--- about/about.js
+++ about/about.js
@@ -1,9 +1,20 @@
+import articles from "../articles.js";
 import { navAndFooter, getArticles } from "../utility.js";
+
+const articlesSection = document.getElementById('articles-section')
+const viewMoreBtn = document.getElementById('view-more-btn')
+
+viewMoreBtn.addEventListener('click', function() {
+    const restArticles = getArticles().slice(3)
+    if (!articlesSection.innerHTML.includes(restArticles.join(''))) {
+        articlesSection.innerHTML += restArticles.join('')
+    }
+})
 
 function render() {
     navAndFooter()
     const firstThreeArticles = getArticles().slice(0, 3)
-    document.getElementById('articles-section').innerHTML = firstThreeArticles.join('')
+    articlesSection.innerHTML = firstThreeArticles.join('')
 }
 
 render()
--- home/index.js
+++ home/index.js
@@ -1,8 +1,45 @@
 import { navAndFooter, getArticles } from "../utility.js";
 
-function render() {
+/* =================
+    DOM Elements
+==================== */
+const articlesSection = document.getElementById('articles-section')
+
+/* ==================
+    MEDIA QUERIES
+===================== */
+const smallScreenSize = window.matchMedia('(max-width: 649px)')
+
+/* ====================
+    MEDIA QUERY MATCH
+======================= */
+if(smallScreenSize.matches) {
+    // LOAD NAV BAR
     navAndFooter()
-    document.getElementById('articles-section').innerHTML = getArticles().join('')
+
+    // INSERT 1ST 3 ARTICLES
+    const firstThreePosts = getArticles().slice(0, 3)
+    articlesSection.innerHTML = firstThreePosts.join('')
+
+    // CREATE VIEW MORE BTN
+    const newBtn = document.createElement('button')
+    newBtn.textContent = 'View More'
+    newBtn.classList.add('view-more-btn')
+    newBtn.id = 'view-more-btn'
+    articlesSection.after(newBtn)
+} else {
+    navAndFooter()
+    articlesSection.innerHTML = getArticles().join('')
 }
 
-render()
+/* =================================
+    EVENT LISTNER FOR VIEW MORE BTN
+==================================== */
+document.getElementById('view-more-btn').addEventListener('click', loadRestPosts)
+
+function loadRestPosts() {
+    const restPosts = getArticles().slice(3)
+    if (!articlesSection.innerHTML.includes(restPosts.join(''))) {
+        articlesSection.innerHTML += restPosts.join('')
+    }
+}

3d86bdbb1773561f15986a441b6915e032b75215
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-09 05:31:21 +0530

    Added screen size tracking
    made javascript more responsive by adding media query tracking.

--- home/index.js
+++ home/index.js
@@ -4,42 +4,54 @@ import { navAndFooter, getArticles } from "../utility.js";
     DOM Elements
 ==================== */
 const articlesSection = document.getElementById('articles-section')
+const viewMoreBtnContainer = document.getElementById('view-more-btn-container')
 
 /* ==================
     MEDIA QUERIES
 ===================== */
-const smallScreenSize = window.matchMedia('(max-width: 649px)')
+const mediaQuery = window.matchMedia('(max-width: 649px)')
 
-/* ====================
-    MEDIA QUERY MATCH
-======================= */
-if(smallScreenSize.matches) {
-    // LOAD NAV BAR
-    navAndFooter()
+function trackScreenSizeChange(e) {
+    if(e.matches) {
+        // LOAD NAV BAR
+        navAndFooter()
 
-    // INSERT 1ST 3 ARTICLES
-    const firstThreePosts = getArticles().slice(0, 3)
-    articlesSection.innerHTML = firstThreePosts.join('')
+        // INSERT 1ST 3 ARTICLES
+        const firstThreePosts = getArticles().slice(0, 3)
+        articlesSection.innerHTML = firstThreePosts.join('')
 
-    // CREATE VIEW MORE BTN
-    const newBtn = document.createElement('button')
-    newBtn.textContent = 'View More'
-    newBtn.classList.add('view-more-btn')
-    newBtn.id = 'view-more-btn'
-    articlesSection.after(newBtn)
-} else {
-    navAndFooter()
-    articlesSection.innerHTML = getArticles().join('')
+        // CREATE VIEW MORE BTN
+        viewMoreBtnContainer.innerHTML = `
+            <button class='view-more-btn' id='view-more-btn' >
+                View More
+            </button>
+        `
+
+        // EVENT LISTNER FOR VIEW MORE BTN
+        const viewMoreBtn = document.getElementById('view-more-btn')
+        viewMoreBtn.addEventListener('click', loadRestPosts)
+
+        function loadRestPosts() {
+            const restPosts = getArticles().slice(3)
+            if (!articlesSection.innerHTML.includes(restPosts.join(''))) {
+                articlesSection.innerHTML += restPosts.join('')
+            }
+        }
+    } else {
+        navAndFooter()
+        articlesSection.innerHTML = getArticles().join('')
+
+        // REMOVE VIEW MORE BTN
+        const viewMoreBtn = document.getElementById('view-more-btn')
+        viewMoreBtn.remove()
+
+    }
 }
+
+mediaQuery.addEventListener('change', trackScreenSizeChange)
+
+trackScreenSizeChange(mediaQuery)
 
 /* =================================
     EVENT LISTNER FOR VIEW MORE BTN
 ==================================== */
-document.getElementById('view-more-btn').addEventListener('click', loadRestPosts)
-
-function loadRestPosts() {
-    const restPosts = getArticles().slice(3)
-    if (!articlesSection.innerHTML.includes(restPosts.join(''))) {
-        articlesSection.innerHTML += restPosts.join('')
-    }
-}
